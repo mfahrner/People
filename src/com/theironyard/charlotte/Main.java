ac48c90fe121019d82493140980086f795eaf972
michael fahrner <Mike Fahrner>
2016-08-12 13:51:48 -0400

    sorted, needs Json

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,6 +3,7 @@ package com.theironyard.charlotte;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,13 +17,12 @@ public class Main {
         File f = new File("people.txt");
 
         Scanner fileScanner = new Scanner(f);
-        ArrayList<Person> arrayPerson = new ArrayList<>();
+
 
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split(",");
             Person filePeople = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
-            arrayPerson.add(filePeople);
             // put if absent add empty arraylist as value of hashmap
             // personHash.put(columns[4], arrayPerson);
             // I think this is storing properly?
@@ -30,8 +30,11 @@ public class Main {
             // could be a problem with the way I am calling
             personHash.putIfAbsent(columns[4], new ArrayList<>());
             personHash.get(columns[4]).add(filePeople);
-
+            Collections.sort(personHash.get(columns[4]));
         }
+
+
+
         System.out.println(personHash);
 
 
--- src/com/theironyard/charlotte/Person.java
+++ src/com/theironyard/charlotte/Person.java
@@ -3,7 +3,7 @@ package com.theironyard.charlotte;
 /**
  * Created by mfahrner on 8/11/16.
  */
-public class Person {
+public class Person implements Comparable {
     String id;
     String fName;
     String lName;
@@ -20,15 +20,63 @@ public class Person {
         this.ip = ip;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
     @Override
     public String toString() {
         return String.format("%s %s from %s\n", fName, lName, country);
     }
 
-    // public int compareTo(Object o) {
-       // Person p = (Person) o;
-        // return lName.compareTo(p.lName);
-    // }
+     public int compareTo(Object o) {
+        Person p = (Person) o;
+         return lName.compareTo(p.lName);
+     }
 }
 
 
