2fdcdb5cdfc16c7765757477c98912e4fedf6586
michael fahrner <Mike Fahrner>
2016-08-12 14:12:37 -0400

    I think just need to refact

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,7 +1,11 @@
 package com.theironyard.charlotte;
 
+import jodd.json.JsonSerializer;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,6 +19,7 @@ public class Main {
 	// write your code here
 
         File f = new File("people.txt");
+        File fjson = new File("people.json");
 
         Scanner fileScanner = new Scanner(f);
 
@@ -23,18 +28,22 @@ public class Main {
             String line = fileScanner.nextLine();
             String[] columns = line.split(",");
             Person filePeople = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
-            // put if absent add empty arraylist as value of hashmap
-            // personHash.put(columns[4], arrayPerson);
-            // I think this is storing properly?
-            // tried to use .get to print specific "keys" but got one long array
-            // could be a problem with the way I am calling
             personHash.putIfAbsent(columns[4], new ArrayList<>());
             personHash.get(columns[4]).add(filePeople);
             Collections.sort(personHash.get(columns[4]));
         }
 
+        // write Json
+        JsonSerializer serializer = new JsonSerializer();
+        String jsonString = serializer.serialize(personHash);
+        try {
+            FileWriter fw = new FileWriter(fjson);
+            fw.write(jsonString);
+            fw.close();
+        } catch (IOException e) {
 
-
+        }
+        
         System.out.println(personHash);
 
 
